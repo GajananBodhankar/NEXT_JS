beb4ef2eb2065819bc0cc52c96b117a81f99cb84
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-05-31 10:19:06 +0530

    Added some comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@
 // The middleware data in next js is seen in
 // the terminal where the next js is running not in the console.log
 // of the DOM
-
 export function middleware() {
   console.log("in middleware");
 }
