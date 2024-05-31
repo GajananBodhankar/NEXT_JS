4afcf33f0898ecce355adfede864209481e1e7c1
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-05-31 10:18:50 +0530

    Added some comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,9 @@
+// here, middleware is one more feature of NextJS
+// Same as middleware of express js
+// The middleware data in next js is seen in
+// the terminal where the next js is running not in the console.log
+// of the DOM
+
 export function middleware() {
   console.log("in middleware");
 }
