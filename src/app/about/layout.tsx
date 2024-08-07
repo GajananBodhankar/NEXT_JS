953152ae91f35a469894bcd7d062d57ee5ab60d4
Gajanan Bodhankar <124874678+GajananBodhankar@users.noreply.github.com>
2024-08-07 19:57:48 +0530

    Update layout.tsx

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-
+// layout is something that will be available throughout its childrens
 function Layout({ children }: any) {
   return (
     <div>
